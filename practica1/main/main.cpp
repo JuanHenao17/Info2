e06c2e0ecfe6c8628feffbbc100cb3663690b722
JuanHenao17 <juanfernando.henao@udea.edu.co>
2024-08-31 20:15:49 -0500

    ejercicios 9-10 y 21-25 realizados

--- practica1/main/main.cpp
+++ practica1/main/main.cpp
@@ -126,8 +126,148 @@ int ejercicio8() {
     return 0;
 }
 
+int ejercicio9() {
+
+    const float pi = 3.1416;
+    int a;
+
+    cout << "Ingrese el radio del circulo:" << endl;
+    cin >> a;
+
+    float perimetro = 2*pi*a;
+    float area = pi * a * a;
+
+    cout << endl << "Perimetro: " << perimetro << endl;
+    cout << endl << "Area: " << area << endl;
+
+    return 0;
+}
+
+int ejercicio10(){
+
+    int a;
+
+    do{
+        cout << "Ingrese un numero entre 1 y 100:" << endl;
+        cin >> a;
+
+    }while(a>100);
+
+    for (int i = 1; i <= 100; ++i) {
+        if (i%a == 0){
+            cout << endl << i << endl;}
+
+    }
+
+    return 0;
+}
+
+int ejercicio21(){
+
+    char letra;
+    cout << "Ingrese una letra: " << endl;
+    cin >> letra;
+    if (isupper(letra)){
+        letra = tolower(letra); }
+    else{
+        letra = toupper(letra);
+    }
+
+    cout << endl << "Letra convertida: " << letra << endl;
+
+    return 0;
+}
+
+int ejercicio22(){
+
+    int segs, horas, mins;
+    cout << "Ingrese una cantidad de segundos: " << endl;
+    cin >> segs;
+
+    horas = segs/3600;
+    segs -= (3600*horas);
+
+    mins = segs/60;
+    segs -= (60*mins);
+
+    cout << endl << horas << ":" << mins << ":" << segs << endl;
+
+    return 0;
+}
+
+int ejercicio23() {
+
+    int a, b, mcd;
+    cout << "Ingrese dos numeros:" << endl;
+    cin >> a;
+    cin >> b;
+
+    for (int i = 1; i <= a; ++i) {
+        if (a%i == 0){
+            if (b%i == 0){
+                mcd = i;}
+        }
+    }
+
+    int mcm = (a*b)/mcd;
+    cout << endl << "El MCM de " << a << " y " << b << " es: " << mcm << endl;
+
+    return 0;
+}
+
+int ejercicio24() {
+
+    int fila;
+    cout << "Ingrese un numero:" << endl;
+    cin >> fila;
+    int espacios = fila - 2;
+    cout << endl;
+
+    for(int i = 0; i < fila; ++i){
+        cout << "+";
+    }
+    cout << endl;
+
+    for (int i = 0; i < espacios; ++i){
+        cout << "+";
+        for (int j = 0; j < espacios; ++j){
+            cout << " ";
+        }
+        cout << "+";
+        cout << endl;
+    }
+
+    for(int i = 0; i < fila; ++i){
+        cout << "+";
+    }
+
+    return 0;
+}
+
+int ejercicio25() {
+
+    long long int a;
+    cout << "Ingrese un numero:" << endl;
+    cin >> a;
+    int cont = 0;
+
+    if(a==0){
+        cout << endl << "0 tiene un digito." << endl;
+        return 0;
+    }
+
+    while (a>0) {
+        a /= 10;
+        cont++;
+
+    }
+    cout << endl << "Tiene " << cont << " digitos." << endl;
+    return 0;
+}
+
 int main()
 {
-    ejercicio8();
+    ejercicio25();
 }
+
 
