0ff03e5f0e687b0af425de82d19c0c7345ff531f
JuanHenao17 <juanfernando.henao@udea.edu.co>
2024-09-14 02:44:21 -0500

    problemas 2 y 6 solucionados

--- practica2/main.cpp
+++ practica2/main.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 void fun_a(int *px, int *py);
 void fun_b(int a[], int tam);
 void fun_c_corregida(double a[], int n, double *promedio, double *suma);
+void problema2();
+void problema6();
 
 int main()
 {
-    int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    cout << "antes: ";
-    for (int i = 0; i < 10; ++i) {
-        cout << array[i] << " " ;
-    }
-
-    cout << endl;
-
-    fun_b(array, 10);
-    cout << "despues: ";
-    for (int i = 0; i < 10; ++i) {
-        cout << array[i] << " ";
-    }
-
 
     //Caso de prueba fun_c
     double promedio, suma;
@@ -32,6 +21,9 @@ int main()
     fun_c_corregida(arrayb, 10, ap_promedio, ap_suma);
     cout << endl << "El promedio esperado es: 5.529" << endl;
     cout << "El promedio resultante es: " << promedio << endl;
+
+    problema6();
+
 }
 
 void fun_a(int *px,int*py){ int
@@ -55,4 +47,50 @@ void fun_c_corregida(double a[], int n, double *promedio, double *suma){
         *suma += *(a + i);
     }
     *promedio = *suma / n;
+}
+
+void problema2(){
+
+    srand(time(0));
+    const int tam = 200;
+    char letras[tam];
+
+    for(int i=0; i<tam; i++){
+        letras[i] = 'A' + rand() % 26;
+    }
+
+    for(int i=0; i<tam; i++){
+        cout << letras[i];
+    }
+
+     cout << endl;
+
+    int cont[26]={0};
+
+     for(int i=0; i<tam; i++){
+         cont[letras[i]-'A']++;
+    }
+
+    for (int i = 0; i < 26; ++i){
+        cout << char('A' + i) << ": " << cont[i] << endl;
+        }
+}
+
+void problema6(){
+
+    string cadena;
+
+    cout << "Introduzca la cadena de caracteres: " << endl;
+    getline(cin, cadena);
+
+    string copia = cadena;
+    for(int i=0; i< cadena.length(); i++){
+        if (copia[i] >= 'a' && copia[i] <= 'z'){
+            copia[i] = toupper(copia[i]);
+        }
+    }
+
+    cout << endl << "Original: " << cadena << endl;
+    cout << "En mayuscula: " << copia << endl;
+
 }
